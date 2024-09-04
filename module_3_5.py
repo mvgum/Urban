606b252f26e737d06e46adfd4eca4472882a7188
mvgum <118298719+mvgum@users.noreply.github.com>
2024-09-04 20:21:06 +0500

    Update module_3_5.py
    Исправил в соответствии с заданными условиями.

--- module_3_5.py
+++ module_3_5.py
@@ -1,11 +1,12 @@
 # Самостоятельная работа по уроку "Рекурсия".
 
-
 def get_multiplied_digits(number):
-    if number == 1:
-        return 1
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
     else:
-        return number * get_multiplied_digits(number - 1)
+        return first
 
 
-print(get_multiplied_digits(6))
+print(get_multiplied_digits(40203))

ad24f10c038cb30b6dcb65996e095bcd5ba96bf5
mvgum <118298719+mvgum@users.noreply.github.com>
2024-09-12 21:23:48 +0500

    Доработал в соответствии с замечанием.

--- module_5_3.py
+++ module_5_3.py
@@ -9,28 +9,70 @@ class House:
         return self.number_of_floors
 
     def __str__(self):
-        return (f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            if isinstance(other.number_of_floors, int):
+                return self.number_of_floors == other.number_of_floors
+            else:
+                print(f'"Объект {other.number_of_floors} не является целым числом!"')
+        else:
+            print("Второй объект не является элементом класса House!")
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            if isinstance(other.number_of_floors, int):
+                return self.number_of_floors < other.number_of_floors
+            else:
+                print(f'"Объект {other.number_of_floors} не является целым числом!"')
+        else:
+            print("Второй объект не является элементом класса House!")
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, House):
+            if isinstance(other.number_of_floors, int):
+                return self.number_of_floors <= other.number_of_floors
+            else:
+                print(f'"Объект {other.number_of_floors} не является целым числом!"')
+        else:
+            print("Второй объект не является элементом класса House!")
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other, House):
+            if isinstance(other.number_of_floors, int):
+                return self.number_of_floors > other.number_of_floors
+            else:
+                print(f'"Объект {other.number_of_floors} не является целым числом!"')
+        else:
+            print("Второй объект не является элементом класса House!")
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, House):
+            if isinstance(other.number_of_floors, int):
+                return self.number_of_floors >= other.number_of_floors
+            else:
+                print(f'"Объект {other.number_of_floors} не является целым числом!"')
+        else:
+            print("Второй объект не является элементом класса House!")
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            if isinstance(other.number_of_floors, int):
+                return self.number_of_floors != other.number_of_floors
+            else:
+                print(f'"Объект {other.number_of_floors} не является целым числом!"')
+        else:
+            print("Второй объект не является элементом класса House!")
 
     def __add__(self, value):
-        self.number_of_floors += value
+        if isinstance(self, House):
+            if isinstance(self.number_of_floors, int):
+                self.number_of_floors += value
+            else:
+                print(f'"Объект {self.number_of_floors} не является целым числом!"')
+        else:
+            print("Второй объект не является элементом класса House!")
         return self
 
     def __radd__(self, value):
@@ -47,21 +89,21 @@ h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 
-print(h1 == h2) # __eq__
+print(h1 == h2)  # __eq__
 
-h1 = h1 + 10 # __add__
+h1 = h1 + 10  # __add__
 print(h1)
 print(h1 == h2)
 
-h1 += 10 # __iadd__
+h1 += 10  # __iadd__
 print(h1)
 
-h2 = 10 + h2 # __radd__
+h2 = 10 + h2  # __radd__
 print(h2)
 
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
 

7f7339b7dec4ff31383db139b150d4079ac210a1
mvgum <118298719+mvgum@users.noreply.github.com>
2024-09-24 22:45:43 +0500

    х/з 3 модуля 6

---
+++ module_6_3.py
@@ -0,0 +1,50 @@
+# Домашнее задание по теме "Множественное наследование"
+
+class Horse:
+    x_distance = 0
+    sound = 'Frrr'
+
+    def __init__(self):
+        pass
+
+    def run(self, dx):
+        self.x_distance +=dx
+
+
+class Eagle:
+    y_distance = 0
+    sound = 'I train, eat, sleep, and repeat'
+
+    def __init__(self):
+        pass
+
+    def fly(self, dy):
+        self.y_distance += dy
+
+
+class Pegasus(Eagle, Horse):
+    
+    def __init__(self):
+        super().__init__()
+        self.sound = super().sound
+
+    def move(self, dx, dy):
+        self.run(dx)
+        self.fly(dy)
+
+    def get_pos(self):
+        return (self.x_distance, self.y_distance)
+
+    def voice(self):
+        print(self.sound)
+
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()

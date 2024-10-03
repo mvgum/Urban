5c6663e71e77ce5b6be467b43b0b32ca203f91db
mvgum <118298719+mvgum@users.noreply.github.com>
2024-10-03 20:38:26 +0500

    доработанное д/з 5 модуля 7

--- module_7_5.py
+++ module_7_5.py
@@ -1,4 +1,5 @@
 # Домашнее задание по теме "Файлы в операционной системе".
+
 import os
 import time
 
@@ -6,9 +7,11 @@ directory = '.'
 
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.getcwd()
+        filepath = os.path.join(root, file)
         filetime = os.path.getmtime(filepath)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.path.getsize(file)
-        parent_dir = os.path.dirname(file)
-        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, '
+              f'Размер: {filesize} байт, Время изменения: {formatted_time}, '
+              f'Родительская директория: {parent_dir}')

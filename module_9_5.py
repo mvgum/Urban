9763c0b88c6c226fd97e9e642f58bb88c2fe5d08
mvgum <118298719+mvgum@users.noreply.github.com>
2024-10-09 22:02:09 +0500

    д/з 5 модуля 9

---
+++ module_9_5.py
@@ -0,0 +1,54 @@
+# Домашнее задание по теме "Итераторы".
+
+class StepValueError(ValueError):
+    pass
+
+class Iterator:
+    def __init__(self, start, stop, step=1):
+        self.start = start
+        self.stop = stop
+        if step == 0:
+            raise StepValueError('шаг не может быть равен 0')
+        else:
+            self.step = step
+
+    def __iter__(self):
+        self.pointer = self.start
+        return self
+
+    def __next__(self):
+        variable = self.pointer
+        self.pointer += self.step
+        if self.step > 0 and variable > self.stop:
+            raise StopIteration()
+        elif self.step < 0 and variable < self.stop:
+            raise StopIteration()
+        else:
+            return variable
+
+
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
+
+
+for i in iter2:
+    print(i, end=' ')
+    print()
+for i in iter3:
+    print(i, end=' ')
+    print()
+for i in iter4:
+    print(i, end=' ')
+    print()
+for i in iter5:
+    print(i, end=' ')
+    print()

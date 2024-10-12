28c35c50504e03e573897d9e9aac6eb93c5a2f0d
mvgum <118298719+mvgum@users.noreply.github.com>
2024-10-12 15:57:17 +0500

    д/з 1 модуля 10

--- module_10_1.py
+++ module_10_1.py
@@ -2,6 +2,7 @@
 
 from time import sleep
 from threading import Thread
+from datetime import datetime
 
 
 def write_words(word_count, file_name):
@@ -14,10 +15,21 @@ def write_words(word_count, file_name):
     print(f'Завершилась запись в файл {file_name}.')
 
 
-thr_first = Thread(target=write_words, args=(10, "example1.txt"))
-thr_second = Thread(target=write_words, args=(30, "example2.txt"))
-thr_third = Thread(target=write_words, args=(200, "example3.txt"))
-thr_fourth = Thread(target=write_words, args=(100, "example4.txt"))
+time_start_1 = datetime.now()
+write_words(10, "example1.txt")
+write_words(30, "example2.txt")
+write_words(200, "example3.txt")
+write_words(100, "example4.txt")
+time_end_1 = datetime.now()
+timer_1 = time_end_1 - time_start_1
+print(timer_1)
+
+
+time_start_2 = datetime.now()
+thr_first = Thread(target=write_words, args=(10, "example5.txt"))
+thr_second = Thread(target=write_words, args=(30, "example6.txt"))
+thr_third = Thread(target=write_words, args=(200, "example4.txt"))
+thr_fourth = Thread(target=write_words, args=(100, "example8.txt"))
 
 thr_first.start()
 thr_second.start()
@@ -28,3 +40,6 @@ thr_first.join()
 thr_second.join()
 thr_third.join()
 thr_fourth.join()
+time_end_2 = datetime.now()
+timer_2 = time_end_2 - time_start_2
+print(timer_2)

671cdb05b5259cfc249a97763da558f83ed9a441
mvgum <118298719+mvgum@users.noreply.github.com>
2024-11-24 17:19:29 +0500

    Add files via upload

--- module_16_2.py
+++ module_16_2.py
@@ -1,5 +1,6 @@
 # Домашнее задание по теме "Валидация данных".
 from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -10,7 +11,7 @@ async def read_user(user_id: int = Path(ge=1, le=100, description="Enter User ID
 
 
 @app.get("/user/{username}/{age}")
-async def read_name(username: str = Path(min_length=5, max_length=20,
-                                         description="Enter username", example='UrbanUser'),
+async def read_name(username: Annotated[str, Path(min_length=5, max_length=20,
+                                         description="Enter username", example='UrbanUser')],
                     age: int = Path(ge=18, le=120, description="Enter age", example=24)):
     return {f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
